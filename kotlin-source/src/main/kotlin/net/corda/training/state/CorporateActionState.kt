9e5dc4da35e9cc1006a96f6ddccb0623bb7a810b
taras <taras.shvyryd@intellecteu.com>
2018-10-24 23:45:36 +0300

    create a state for corporate action use case

---
+++ kotlin-source/src/main/kotlin/net/corda/training/state/CorporateActionState.kt
@@ -0,0 +1,19 @@
+package net.corda.training.state
+
+import net.corda.core.contracts.Amount
+import net.corda.core.contracts.LinearState
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.identity.AbstractParty
+import net.corda.core.identity.Party
+import java.util.*
+
+class CorporateActionState(val owner: Party,
+                           val currency: Currency,
+                           val investments: Map<Party, Amount<Currency>>,
+                           val profit: Double,
+                           override val linearId: UniqueIdentifier): LinearState {
+    override val participants: List<Party>
+        get() = investments.keys.toList().plusElement(owner)
+
+    fun dividend(party: Party) = investments[party]!!.plus(Amount((investments[party]!!.quantity * profit).toLong(), currency))
+}
